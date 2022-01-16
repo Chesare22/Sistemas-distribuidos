efd824a6051bb2e5e074c8596e6e2ec6074ddb2d
César González <chesarglez429@gmail.com>
2022-01-16 13:18:59 -0600

    :tada: Create new Elixir application

---
+++ connect_to_java_socket/lib/connect_to_java_socket.ex
@@ -0,0 +1,18 @@
+defmodule ConnectToJavaSocket do
+  @moduledoc """
+  Documentation for `ConnectToJavaSocket`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ConnectToJavaSocket.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ connect_to_java_socket/test/connect_to_java_socket_test.exs
@@ -0,0 +1,8 @@
+defmodule ConnectToJavaSocketTest do
+  use ExUnit.Case
+  doctest ConnectToJavaSocket
+
+  test "greets the world" do
+    assert ConnectToJavaSocket.hello() == :world
+  end
+end
