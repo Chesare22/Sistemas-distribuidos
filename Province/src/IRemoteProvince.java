f414495abf197574dbe9df6693eec2828a2efa78
César González <chesarglez429@gmail.com>
2022-01-25 16:03:28 -0600

    :label: Remove redundant public acces on iterface

--- Province/src/IRemoteProvince.java
+++ Province/src/IRemoteProvince.java
@@ -3,15 +3,15 @@ import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 public interface IRemoteProvince extends Remote {
-    public int save(Province p) throws RemoteException;
+    int save(Province p) throws RemoteException;
 
-    public int update(Province p) throws RemoteException;
+    int update(Province p) throws RemoteException;
 
-    public int delete(Province p) throws RemoteException;
+    int delete(Province p) throws RemoteException;
 
-    public void deleteAll() throws RemoteException;
+    void deleteAll() throws RemoteException;
 
-    public ArrayList findAll() throws RemoteException;
+    ArrayList findAll() throws RemoteException;
 
-    public ArrayList findByName(String criteria) throws RemoteException;
+    ArrayList findByName(String criteria) throws RemoteException;
 }
